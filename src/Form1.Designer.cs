f7988e5438799b31770d15e6764446ef607a63c0
tommilostny <tommilostny@live.com>
2020-11-24 20:25:31 +0100

    Added Oxyplot, stem graphs to visualize the audio files

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -35,6 +35,18 @@ namespace src
             this.label1 = new System.Windows.Forms.Label();
             this.buttonPlay = new System.Windows.Forms.Button();
             this.buttonStop = new System.Windows.Forms.Button();
+            this.plotViewMaskOffTone = new OxyPlot.WindowsForms.PlotView();
+            this.plotView2 = new OxyPlot.WindowsForms.PlotView();
+            this.plotViewMaskOnTone = new OxyPlot.WindowsForms.PlotView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -76,17 +88,120 @@ namespace src
             this.buttonStop.Text = "Stop";
             this.buttonStop.Click += new System.EventHandler(this.OnButtonStopClick);
             // 
+            // plotViewMaskOffTone
+            // 
+            this.plotViewMaskOffTone.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotViewMaskOffTone.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.plotViewMaskOffTone.Location = new System.Drawing.Point(3, 3);
+            this.plotViewMaskOffTone.Name = "plotViewMaskOffTone";
+            this.plotViewMaskOffTone.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewMaskOffTone.Size = new System.Drawing.Size(846, 303);
+            this.plotViewMaskOffTone.TabIndex = 5;
+            this.plotViewMaskOffTone.Text = "plotViewMaskOffTone";
+            this.plotViewMaskOffTone.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewMaskOffTone.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewMaskOffTone.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // plotView2
+            // 
+            this.plotView2.Location = new System.Drawing.Point(0, 0);
+            this.plotView2.Name = "plotView2";
+            this.plotView2.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView2.Size = new System.Drawing.Size(0, 0);
+            this.plotView2.TabIndex = 7;
+            this.plotView2.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView2.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView2.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // plotViewMaskOnTone
+            // 
+            this.plotViewMaskOnTone.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotViewMaskOnTone.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.plotViewMaskOnTone.Location = new System.Drawing.Point(3, 3);
+            this.plotViewMaskOnTone.Name = "plotViewMaskOnTone";
+            this.plotViewMaskOnTone.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewMaskOnTone.Size = new System.Drawing.Size(846, 309);
+            this.plotViewMaskOnTone.TabIndex = 6;
+            this.plotViewMaskOnTone.Text = "plotView1";
+            this.plotViewMaskOnTone.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewMaskOnTone.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewMaskOnTone.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(218, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.plotViewMaskOffTone);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.plotViewMaskOnTone);
+            this.splitContainer1.Size = new System.Drawing.Size(852, 629);
+            this.splitContainer1.SplitterDistance = 309;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 213);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(200, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 10;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Interval length: 1s";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Replot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ReplotButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.plotView2);
             this.Controls.Add(this.buttonPlay);
             this.Controls.Add(this.buttonStop);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,6 +212,13 @@ namespace src
         private Label label1;
         private Button buttonPlay;
         private Button buttonStop;
+        private OxyPlot.WindowsForms.PlotView plotViewMaskOffTone;
+        private OxyPlot.WindowsForms.PlotView plotView2;
+        private OxyPlot.WindowsForms.PlotView plotViewMaskOnTone;
+        private SplitContainer splitContainer1;
+        private TrackBar trackBar1;
+        private Label label2;
+        private Button button1;
     }
 }
 
--- src/Form1.cs
+++ src/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using NAudio.Wave;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace src
@@ -15,13 +11,91 @@ namespace src
     {
         private WaveOutEvent outputDevice;
         private AudioFileReader audioFile;
-
         private string SelectedFile => $@"..\..\..\..\audio\{comboBox1.Items[comboBox1.SelectedIndex]}.wav";
+
+        private record PlotData
+        {
+            public string PlotTitle { get; init; }
+            public string FileName { get; init; }
+            public string FullFilePath => $@"..\..\..\..\audio\{FileName}";
+        }
+
+        private readonly PlotData maskOffTone;
+        private readonly PlotData maskOnTone;
 
         public Form1()
         {
             InitializeComponent();
             comboBox1.SelectedIndex = 0;
+
+            maskOffTone = new PlotData
+            {
+                PlotTitle = "Mask off tone",
+                FileName = "maskoff_tone.wav"
+            };
+            maskOnTone = new PlotData
+            {
+                PlotTitle = "Mask on tone",
+                FileName = "maskon_tone.wav"
+            };
+
+            plotViewMaskOffTone.Model = PlotWavFile(maskOffTone);
+            plotViewMaskOnTone.Model = PlotWavFile(maskOnTone);
+        }
+
+        //16000 = 1s (Fs = 16kHz)
+        private static DataPoint[] LoadAudioSamples(string filePath, int samplesCount = 16000)
+        {
+            var datapoints = new DataPoint[samplesCount];
+            var samples = new short[samplesCount];
+
+            using (var waveFileReader = new WaveFileReader(filePath))
+            {
+                byte[] buffer = new byte[samplesCount * 2];
+                waveFileReader.Read(buffer, 0, buffer.Length);
+                Buffer.BlockCopy(buffer, 0, samples, 0, samplesCount * 2);
+            }
+
+            for (var i = 0; i < samples.Length; i++)
+            {
+                datapoints[i] = new DataPoint(i, samples[i]);
+            }
+            return datapoints;
+        }
+
+        private static PlotModel PlotWavFile(PlotData plotData, double seconds = 1.0)
+        {
+            var pm = new PlotModel
+            {
+                Title = plotData.PlotTitle,
+                Subtitle = plotData.FileName,
+                Background = OxyColors.White,
+            };
+
+            var x_axis = new LinearAxis
+            {
+                Maximum = seconds * 16000,
+                Minimum = 0,
+                Position = AxisPosition.Bottom
+            };
+            var y_axis = new LinearAxis
+            {
+                Maximum = 1100,
+                Minimum = -1100,
+                Position = AxisPosition.Left
+            };
+            pm.Axes.Add(x_axis);
+            pm.Axes.Add(y_axis);
+
+            var stemSeries = new StemSeries
+            {
+                MarkerStroke = OxyColors.Green,
+                MarkerType = MarkerType.Circle
+            };
+            stemSeries.Points.AddRange(LoadAudioSamples(plotData.FullFilePath, (int)(seconds * 16000)));
+            pm.Series.Add(stemSeries);
+
+            return pm;
         }
 
         private void OnButtonStopClick(object sender, EventArgs e)
@@ -50,6 +124,18 @@ namespace src
             outputDevice = null;
             audioFile.Dispose();
             audioFile = null;
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label2.Text = $"Interval length: {Convert.ToDouble(trackBar1.Value) / 10}s";
+        }
+
+        private void ReplotButton_Click(object sender, EventArgs e)
+        {
+            var seconds = Convert.ToDouble(trackBar1.Value) / 10;
+            plotViewMaskOffTone.Model = PlotWavFile(maskOffTone, seconds);
+            plotViewMaskOnTone.Model = PlotWavFile(maskOnTone, seconds);
         }
     }
 }
